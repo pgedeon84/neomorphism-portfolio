79fbd2b406cb79d9a36d5903de857fe457b4c638
Patrice Gedeon <pgedeon84@gmail.com>
2021-05-13 16:22:20 -0400

    media queries for responsiveness complete

--- js/main.js
+++ js/main.js
@@ -351,3 +351,10 @@ function bodyScrollingToggle() {
     }
   });
 })();
+
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".preloader").style.display = "none";
+  }, 600);
+});
